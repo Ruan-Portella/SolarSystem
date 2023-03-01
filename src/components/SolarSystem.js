aca9c40944e89e14bf1a8fd9918dc7ba62b30063
Ruan-Portella <ruanmorales29@gmail.br>
2023-03-01 20:42:59 -0300

    Feat: sim

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -2,24 +2,21 @@ import React from 'react';
 import Planets from '../data/planets';
 import PlanetCard from './PlanetCard';
 import Title from './Title';
-import './SolarSystem.css';
 
 class SolarSystem extends React.Component {
   render() {
     return (
-      <div className="Content-Planet">
+      <>
         <div data-testid="solar-system">
           <Title headline="Planetas" />
         </div>
-        <div className="Planet-Cards">
-          {Planets
-            .map((planet) => (<PlanetCard
-              key={ planet.name }
-              planetName={ planet.name }
-              planetImage={ planet.image }
-            />))}
-        </div>
-      </div>
+        {Planets
+          .map((planet) => (<PlanetCard
+            key={ planet.name }
+            planetName={ planet.name }
+            planetImage={ planet.image }
+          />))}
+      </>
     );
   }
 }
